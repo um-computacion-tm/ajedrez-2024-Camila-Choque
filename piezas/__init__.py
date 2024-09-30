4a4ce12a75d4430abb157e7662956309cb0b4ba0
Camila-Choque <m.choque@alumno.um.edu.ar>
2024-09-30 11:10:55 -0300

    refactorizando

--- piezas/__init__.py
+++ piezas/__init__.py
@@ -1,9 +1,16 @@
-direcciones_king = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-direcciones_horse =  [
-          (2, 1), (2, -1),   #Dos hacia arriba, una a la derecha / izquierda
-          (-2, 1), (-2, -1), #Dos hacia abajo, una a la derecha / izquierda
-          (1, 2), (1, -2),   #Una hacia arriba, dos a la derecha / izquierda
-          (-1, 2), (-1, -2)  #Una hacia abajo, dos a la derecha / izquierda
-        ] 
+direcciones = {
+    'rey': [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)],
+    'caballo': [
+        (2, 1), (2, -1),   # Dos hacia arriba, una a la derecha/izquierda
+        (-2, 1), (-2, -1), # Dos hacia abajo, una a la derecha/izquierda
+        (1, 2), (1, -2),   # Una hacia arriba, dos a la derecha/izquierda
+        (-1, 2), (-1, -2)  # Una hacia abajo, dos a la derecha/izquierda
+    ]
+}
+
+# Uso de las direcciones
+direcciones_king = direcciones['rey']
+direcciones_horse = direcciones['caballo']
+
 direcciones_torre = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 direcciones_alfil =[(1, 1), (1, -1), (-1, 1), (-1, -1)]
